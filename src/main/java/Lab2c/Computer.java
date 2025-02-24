55efbffb9918c35391d990f0840d265b97e4b607
Dragowarfist <fsoegiarto@connect.ust.hk>
2025-02-24 19:31:18 +0800

    Lab 2

---
+++ src/main/java/Lab2c/Computer.java
@@ -0,0 +1,11 @@
+package Lab2c;
+
+public class Computer {
+    protected String secret;
+    public Computer() {
+        secret = "computer Secret";
+    }
+    public void work(){
+        System.out.println("A computer is working.");
+    }
+}
